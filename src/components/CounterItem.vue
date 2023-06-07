982adc19624060b2ebd4472411902a17ca0bb82d
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-07 16:44:59 +0200

    init

---
+++ src/components/CounterItem.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const count = ref(0)
+const double = computed(() => count.value * 2)
+const emit = defineEmits(['increment'])
+const color = computed(() => count.value >= 3 ? 'red' : 'black')
+
+defineProps({
+  prefix: String
+})
+
+function handleClic() {
+  count.value++
+  emit('increment')
+}
+</script>
+
+
+<template>
+  <div>
+    <p class="compteur">
+      {{ prefix }} compteur : {{ count }} *2 = {{ double }}
+
+      <button @click="handleClic">Augmenter</button>
+    </p>
+  </div>
+</template>
+
+
+<style>
+.compteur {
+  color: v-bind(color);
+}
+</style>
